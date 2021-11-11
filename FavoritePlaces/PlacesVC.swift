763dc94da4ec53180873917081cb9d22f5e43398
yusuf <ysfozturk3@gmail.com>
2021-11-11 13:39:53 +0100

    added logaut method

--- FavoritePlaces/PlacesVC.swift
+++ FavoritePlaces/PlacesVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class PlacesVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
@@ -19,10 +20,27 @@ class PlacesVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
         tableView.delegate = self
 
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Logaut", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logautButtonClicked))
     }
     
     @objc func addButtonClicked() {
         
+    }
+    
+    @objc func logautButtonClicked() {
+        PFUser.logOutInBackground { error in
+            if error != nil {
+                let alert = UIAlertController(title: "ERROR!", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                self.performSegue(withIdentifier:"toSignInVC", sender: nil)
+            }
+                    
+                
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
